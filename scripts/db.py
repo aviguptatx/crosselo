2e536094fc4849ceb55c2d72e0a03cd6cc33343c
Avi Gupta <aviguptatx@gmail.com>
2024-09-30 22:36:04 -0700

    remove /rest/v1 from db url due to lib change

--- scripts/db.py
+++ scripts/db.py
@@ -2,5 +2,5 @@ import os
 import supabase
 
 supabase_client = supabase.create_client(
-    os.environ.get("SUPABASE_API_URL"), os.environ.get("SUPABASE_API_KEY")
+    "https://twyzufmqxsqoaqjidwbu.supabase.co", os.environ.get("SUPABASE_API_KEY")
 )
